5693f7f30b3a3d013c9415f6ce6467bd473e5e6c
Dave Lively <davelively14@gmail.com>
2017-03-18 10:14:12 -0400

    Added error checking

--- filteredLS.js
+++ filteredLS.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 // var filteredDirs = [];
 
 fs.readdir(process.argv[2], function(err, list) {
+  if (err) return console.error(err);
+
   list.forEach(function(e) {
     let splitFile = e.split(".");
     if (splitFile.length > 1 && splitFile[splitFile.length - 1] == process.argv[3]) {
--- myFirstAsyncIO.js
+++ myFirstAsyncIO.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 
 fs.readFile(process.argv[2], function (err, contents) {
+  if (err) return console.error(err);
+  
   console.log(contents.toString().split("\n").length - 1);
 });
